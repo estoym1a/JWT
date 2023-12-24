b470f9f96547cf8cfbf85afcbd7ccff07117cf04
estoym1a <nuray.xudadatova04@gmail.com>
2023-12-24 19:02:38 +0400

    "JWT Security"

---
+++ src/test/java/com/jwt/learningsecurity/LearningSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.learningsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
